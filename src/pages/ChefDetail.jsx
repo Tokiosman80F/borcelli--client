9eb2becc08bd9865086a19268587c67a495d387e
Tokiosman80F <tokiosman96@gmail.com>
2023-09-19 00:05:40 +0600

    recipe is displayed

--- src/pages/ChefDetail.jsx
+++ src/pages/ChefDetail.jsx
@@ -5,8 +5,10 @@ const ChefDetail = () => {
   const { description, image_url, name, specialty, recipes } = data;
   console.log(description, name, specialty, recipes);
   return (
-    <div className="px-52 py-10">
-        <h2 className="text-center text-4xl uppercase text-orange-500 py-5">Chef's Review </h2>
+    <div className="px-52 py-5">
+      <h2 className="text-center font-bold text-4xl uppercase text-orange-400 py-10 ">
+        Chef's Review{" "}
+      </h2>
       <div className="card  lg:card-side bg-base-100 shadow-xl">
         <figure className="w-1/2">
           <img src={image_url} alt="Album" className="object-cover h-96" />
@@ -14,7 +16,7 @@ const ChefDetail = () => {
         <div className="card-body w-1/2 ">
           <h2 className="text-3xl font-bold ">{name} </h2>
           <p className="text-xl">{description}</p>
-          
+
           <div className="card-actions justify-end">
             <button className="btn btn-outline bg-orange-500">
               <p className="text-white">Recommended by:</p>
@@ -26,6 +28,36 @@ const ChefDetail = () => {
             </button>
           </div>
         </div>
+      </div>
+      
+      {/* Best Recipe */}
+
+      <h2 className="text-center font-bold text-4xl uppercase text-orange-400 py-10 mt-10">
+        Best Recipes
+      </h2>
+      <div className="grid grid-cols-2 gap-10 justify-between items-end">
+        {recipes.map((recipe, index) => (
+          <div key={index} className="card w-96 bg-base-100 shadow-xl border-2 bottom-1 ">
+            <figure >
+              <img src={recipe.image_url} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{recipe.title}</h2>
+              <p>{recipe.description}</p>
+              <h2 className="card-title">Ingredients</h2>
+              <div className="grid grid-cols-2">
+                {recipe.ingredients.map((ingredient, index) => (
+                  <p key={index}>{`${index + 1}. ${ingredient}`}</p>
+                ))}
+              </div>
+              <div className="card-actions justify-end">
+                <button className="btn bg-orange-400 text-white hover:bg-orange-500">
+                  Rating {recipe.rating}
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
